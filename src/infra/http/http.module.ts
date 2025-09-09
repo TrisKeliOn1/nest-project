0309efd28d24ca1e6d7674d2a967a767883df5ab
hebert <hebertLandim007@outlook.com>
2025-09-09 17:10:23 -0300

    feat(core): implement communication between layers

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -4,6 +4,7 @@ import { AuthenticateController } from './controller/authenticate.controller'
 import { CreateQuestionController } from './controller/create-question.controller'
 import { FetchPrivateQuestionsController } from './controller/fetch-recent-questions.controller'
 import { DatabaseModule } from '../database/database.module'
+import { CreateQuestionUseCase } from '@/domain/forum/application/use-case/create-question'
 
 @Module({
   imports: [DatabaseModule],
@@ -13,5 +14,6 @@ import { DatabaseModule } from '../database/database.module'
     CreateQuestionController,
     FetchPrivateQuestionsController,
   ],
+  providers: [CreateQuestionUseCase],
 })
 export class HttpModule {}
