2895c6405e52d055cad8ce7736299a880638297d
hebert <hebertLandim007@outlook.com>
2025-10-06 18:54:18 -0300

    feat(use-cases): add use case for uploading and creating attachment

--- src/domain/forum/enterprise/entities/attachment.ts
+++ src/domain/forum/enterprise/entities/attachment.ts
@@ -1,21 +1,21 @@
 import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
-interface AttachmentProops {
+interface AttachmentProps {
   title: string
-  link: string
+  url: string
 }
 
-export class Attachment extends Entity<AttachmentProops> {
+export class Attachment extends Entity<AttachmentProps> {
   get title() {
     return this.props.title
   }
 
-  get link() {
-    return this.props.link
+  get url() {
+    return this.props.url
   }
 
-  static create(props: AttachmentProops, id?: UniqueEntityID) {
+  static create(props: AttachmentProps, id?: UniqueEntityID) {
     const attachment = new Attachment(props, id)
 
     return attachment
